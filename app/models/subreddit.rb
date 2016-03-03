eda268ee8a28674ba4e17f42d17bcf3f28d196f2
leslie kimm <leslie.kimm@gmail.com>
2016-03-03 15:33:15 -0800

    Subreddit tests

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -1,2 +1,4 @@
 class Subreddit < ActiveRecord::Base
+    validates :title, presence: true, length: { maximum: 140 }
+	validates :description, presence: true, length: { maximum: 1500 }
 end
--- test/models/subreddit_test.rb
+++ test/models/subreddit_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class SubredditTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def setup
+        @subreddit = Subreddit.new(title: "Example Title",
+                                   description: "example description")
+    end
+
+    test "should be valid" do
+        assert @subreddit.valid?
+    end
+
+    test "title should be present" do
+        @subreddit.title = ""
+        assert_not @subreddit.valid?
+    end
+
+    test "title should be at most 140 characters" do
+        @subreddit.title = "a" * 141
+        assert_not @subreddit.valid?
+    end
+
+    test "description should be present" do
+        @subreddit.description = ""
+        assert_not @subreddit.valid?
+    end
+
+    test "description should be at most 1500 characters" do
+        @subreddit.description = "a" * 1501
+        assert_not @subreddit.valid?
+    end
 end
