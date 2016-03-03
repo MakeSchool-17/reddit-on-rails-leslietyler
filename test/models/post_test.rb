ca83995d21bd7499cd95d8a5e1acd924022680c8
tylerhoffs <tylerhoffman@outlook.com>
2016-03-03 14:58:16 -0800

    Post model tests and validations

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = Post.new(title: "Example Title", content: "example content", points: 0)
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = "   "
+    assert_not @post.valid?
+  end
+
+  test "title should be present" do
+    @post.title = "   "
+    assert_not @post.valid?
+  end
+
+  test "content should be at most 255 characters" do
+    @post.content = "a" * 256
+    assert_not @post.valid?
+  end
+
+  test "title should be at most 140 characters" do
+    @post.title = "a" * 141
+    assert_not @post.valid?
+  end
 end
