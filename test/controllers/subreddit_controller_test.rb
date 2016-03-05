2d60fd91d66325b72ae4d573e4b5861c1a31be07
leslie kimm <leslie.kimm@gmail.com>
2016-03-04 16:51:51 -0800

    Add subreddit controllers

--- test/controllers/subreddit_controller_test.rb
+++ test/controllers/subreddit_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class SubredditControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def setup
+
+    end
+
+    test "should redirect to subreddit from slug" do
+        # access a route with r/slug
+        assert_template "subreddit"
+    end
 end
