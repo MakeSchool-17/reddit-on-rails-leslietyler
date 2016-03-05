177717ba8e2c947891672c644dd59bbd3df22b5c
leslie kimm <leslie.kimm@gmail.com>
2016-03-04 16:50:02 -0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
+  get 'subreddit/new'
+  get 'subreddit/index'
+  get 'subreddit/show'
+  get 'subreddit/edit'
   # root 'welcome#index'
 
   # Example of regular route:
