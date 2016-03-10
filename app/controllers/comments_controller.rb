45514271eeb13d3c712635a466f7789fc31f8de2
leslie kimm <leslie.kimm@gmail.com>
2016-03-10 09:03:13 -0800

    create comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+
+  get 'comments/show'
+
+  get 'comments/edit'
+
+  get 'comments/update'
+
+  get 'comments/create'
+
+  get 'comments/index'
+
     root 'subreddits#new'
     resources :subreddits
 
