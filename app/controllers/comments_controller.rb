011a529265d32320702114a8f86215aa4320419e
leslie kimm <leslie.kimm@gmail.com>
2016-03-11 17:14:14 -0800

    add comments controller actoins

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,43 @@
 class CommentsController < ApplicationController
-  def new
-  end
+    def new
+        @comment = Comment.new
+    end
 
-  def show
-  end
+    def index
+        #@posts = Post.all
+    end
 
-  def edit
-  end
+    def show
+        @current_comment = Comment.find(params[:id])
+    end
 
-  def update
-  end
+    def edit
+        @comment = Comment.find(params[:id])
+    end
 
-  def create
-  end
+    def update
+        @comment = Comment.find(params[:id])
+        if @comment.update_attributes(comment_params)
+            flash[:success] = "Comment updated"
+            redirect_to @comment
+        else
+            render 'edit'
+        end
+    end
 
-  def index
-  end
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.user_id = 1
+        @comment.points = 0
+        # if @comment.save!
+        #     redirect_to subreddit_post_path(@comment.subreddit_id, @post.id)
+        # else
+        #     render 'new'
+        # end
+    end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:content, :user_id)
+        end
 end
