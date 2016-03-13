f42d4853d34924f675e4dd2764005165f00739f4
leslie kimm <leslie.kimm@gmail.com>
2016-03-13 07:30:57 -0700

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,18 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-
-  get 'comments/show'
-
-  get 'comments/edit'
-
-  get 'comments/update'
-
-  get 'comments/create'
-
-  get 'comments/index'
-
     root 'subreddits#new'
     resources :subreddits
 
     resources :subreddits do
-    	resources :posts
+    	resources :posts do
+    	    resources :comments
+    	end
 	end
 
-	resources :posts
+	resources :posts do
+	    resources :comments
+	end
+
+    resources :comments do
+	    resources :comments
+	end
 end
